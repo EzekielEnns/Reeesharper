49ea235963815b0583fb163a34da9d1189d2dc88
ezekielenns <ezekiel.enns@protonmail.com>
2024-05-02 10:44:25 -0300

    init2

---
+++ RpcClient/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace RpcClient;
+
+public class Client
+{
+    /*
+     so there are multiple ways to open a channel for neovim and for msgpack rpc
+     this class is the primary client that impls this spec: 
+        https://github.com/MessagePack-CSharp/MessagePack-CSharp?tab=readme-ov-file#nuget-packages 
+     
+
+     */
+    
+}
